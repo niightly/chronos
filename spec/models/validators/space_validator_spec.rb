ac21d88fba55fec6fa2a65a3d41f22a42dd61176
Thiago Silva <thiagogs@br.ibm.com>
2018-11-29 16:21:30 -0200

    Fix indentation

--- spec/models/validators/space_validator_spec.rb
+++ spec/models/validators/space_validator_spec.rb
@@ -1,21 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe SpaceValidator do
- describe '# validate' do
-  context 'valid document' do
-   subject { FactoryBot.build(:space) }
-   it { expect(subject.save).to be_truthy }
-  end
+	describe '# validate' do
+		context 'valid document' do
+			subject { FactoryBot.build(:space) }
+			it { expect(subject.save).to be_truthy }
+		end
 
-  context 'invalid document' do
-   it 'should return false if missing phones' do
-    doc = FactoryBot.build(:space, phones: [])
-    expect(doc.save).not_to be_truthy
-   end
-   it 'should return false if duplicated phones' do
-    doc = FactoryBot.build(:space, phones: %w[0143211234 0143211234])
-    expect(doc.save).not_to be_truthy
-   end
-  end
- end
+		context 'invalid document' do
+			it 'should return false if missing phones' do
+				doc = FactoryBot.build(:space, phones: [])
+				expect(doc.save).not_to be_truthy
+			end
+			it 'should return false if duplicated phones' do
+				doc = FactoryBot.build(:space, phones: %w[0143211234 0143211234])
+				expect(doc.save).not_to be_truthy
+			end
+		end
+	end
 end
